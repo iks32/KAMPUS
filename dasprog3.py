754d1c27c43166ea5db3820ec3bb81cb310c51dc
iks32 <103190725+iks32@users.noreply.github.com>
2022-09-29 23:12:33 +0700

    1

---
+++ dasprog3.py
@@ -0,0 +1,12 @@
+print("\n------------------------------------")
+print("*** PROGRAM KALKULATOR SEDERHANA ***")
+print("------------------------------------\n")
+nilai1 = int(input("Masukkan Nilai Pertama : "))
+nilai2 = int(input("Masukkan Nilai Kedua : "))
+print("------------------------------------\n")
+penjumlahan = nilai1 + nilai2
+print(nilai1, "+", nilai2, "=", penjumlahan)
+pengurangan = nilai1 - nilai2
+print(nilai1, "-", nilai2, "=", pengurangan)
+perkalian = nilai1 * nilai2
+print(nilai1, "*", nilai2)
---
+++ operatorpembanding.py
@@ -0,0 +1,21 @@
+print("PROGRAM OPERATOR PERBANDINGAN")
+a = int(input("Masukkan nilai a : "))
+b = int(input("Masukkan nilai b : "))
+
+c=a==b
+print("Apakah nilai a == nilai b : %s" % c)
+
+c=a<b
+print("Apakah nilai a < nilai b : %s" %c)
+
+c=a>b
+print("Apakah nilai a > nilai b : %s" %c)
+
+c=a<=b
+print("Apakah nilai a <= nilai b : %s" %c)
+
+c=a>=b
+print("Apakah nilai a >= nilai b : %s" %c)
+
+c=a!=b
+print("Apakah nilai a != nilai b : %s" %c)
