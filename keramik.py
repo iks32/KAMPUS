e3d22a98d36eba466bd4f21af3643e8207352e8f
iks32 <103190725+iks32@users.noreply.github.com>
2022-10-19 15:04:24 +0700

    1

--- keramik.py
+++ keramik.py
@@ -5,3 +5,4 @@ mat2 = int(input("BERAPA KERAMIK YANG SUDAH DIPINDAHKAN :"))
 jumlah = mat1-mat2
 print("\n--------------HASIL----------")
 print("SISAH KERAMIK YANG BELUM DIPINDAHKAN ADALAH  :",format(jumlah))
+##tes
